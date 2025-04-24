068cc55f5f69b9c6c1dd5f1f1a2c71042d950db2
Joseph Ndungu <jothefu@gmail.com>
2025-04-24 20:48:31 +0300

    Update example.php
    Improve example.php form with input sanitization, labels, and user feedback messages

--- example.php
+++ example.php
@@ -1,13 +1,32 @@
 <?php
 require_once 'class.instagram.php';
+
+$message = '';
+$story_html = '';
+
 if (isset($_POST['submit'])) {
-    $story = new instagram_story();
-    $story->getStory($_POST['username']);
+    $username = trim($_POST['username']);
+    if (empty($username)) {
+        $message = "Please enter a username.";
+    } else {
+        $story = new instagram_story();
+        ob_start();
+        $story->getStory($username);
+        $story_html = ob_get_clean();
+    }
 }
 ?>
-<div id="center" style="text-align: center; margin-top: 300px;">
+<div id="center" style="text-align: center; margin-top: 100px;">
     <form action="" method="post">
-        <input name="username">
-        <input type="submit" name="submit">
+        <label for="username">Instagram Username:</label><br>
+        <input id="username" name="username" type="text" placeholder="Enter Instagram username" required>
+        <br><br>
+        <input type="submit" name="submit" value="Get Stories">
     </form>
+    <div style="margin-top: 20px; color: red;">
+        <?php echo htmlspecialchars($message); ?>
+    </div>
+    <div style="margin-top: 20px;">
+        <?php echo $story_html; ?>
+    </div>
 </div>
